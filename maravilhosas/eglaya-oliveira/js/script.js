a2ed48ea90c529859affde4ccd99472e9ecf836f
Eglaya Oliveira <gayahh_sg@hotmail.com>
2019-12-17 12:26:31 -0300

    adicionando projeto final de Eglaya Oliveira

---
+++ maravilhosas/eglaya-oliveira/js/script.js
@@ -0,0 +1,23 @@
+// Modal Sobre a Autora
+
+const modal = document.getElementById('autora_modal');
+const modalBtn = document.getElementById('secao-autora__btn');
+const closeBtn = document.querySelector('.close');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
