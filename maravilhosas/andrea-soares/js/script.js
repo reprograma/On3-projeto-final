9e006fbf5ef5df562ba8124234aa06400537a27b
Andréa Soares <asoaresdev@gmail.com>
2019-12-19 10:09:12 -0300

    Entrega do Projeto Final de Andréa Soares

---
+++ maravilhosas/andrea-soares/js/script.js
@@ -0,0 +1,37 @@
+(function() {
+    var menuResponsivo = document.getElementById("menuResponsivo");
+    var btnResponsivo = document.getElementById("btnResponsivo");
+  
+    btnResponsivo.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.body.scrollTop += 0;
+      if (menuResponsivo.className === "navegacao__itens") {
+        menuResponsivo.className += " ativo";
+      } else {
+        menuResponsivo.className = "navegacao__itens";
+      }
+    });
+  })();
+  
+  window.addEventListener("scroll", function() {
+    if (pageYOffset >= 250) {
+      document.querySelector(".navegacao").style.backgroundColor = "#674582";
+      document.querySelector(".navegacao__itens").style.backgroundColor =
+        "#674582";
+    } else {
+      document.querySelector(".navegacao").style.backgroundColor = "transparent";
+      document.querySelector(".navegacao__itens").style.backgroundColor =
+        "transparent";
+    }
+  });
+
+function typeWriter(elemento){
+  let textoArray = elemento. innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+  setTimeout(() => elemento.innerHTML += letra, 100 * i)
+  });
+}
+let titulo = document.querySelector('.principal__descricao');
+typeWriter(titulo);
+  
