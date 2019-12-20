88790f557ac3286c3167a7876c96dc81920a6daa
Tatiane Chiarello <tatichiarello@gmail.com>
2019-12-20 16:03:37 +0100

    Adicionando projeto final - Tatiane Chiarello

---
+++ maravilhosas/tatiane-chiarello/js/anne-frank.js
@@ -0,0 +1,21 @@
+
+// Menu hamburguer
+
+$(document).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+        });
+    });
+    
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+});
